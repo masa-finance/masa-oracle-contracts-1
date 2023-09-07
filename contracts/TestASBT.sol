e3de8b96f5c20a087a463dc127933fd16cd34f74
Brendan Playford <34052452+teslashibe@users.noreply.github.com>
2023-09-07 12:20:07 -0700

    Initial commit

---
+++ contracts/TestASBT.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@masa-finance/masa-contracts-identity/contracts/tokens/MasaSBTAuthority.sol";
+
+/// @title Test ASBT
+/// @author Masa Finance
+/// @notice Test Soulbound token
+/// @dev Inherits from the ASBT contract.
+contract TestASBT is MasaSBTAuthority {
+    /// @notice Creates a new Test ASBT
+    /// @dev Creates a new Test ASBT, inheriting from the Masa ASBT contract.
+    /// @param admin Administrator of the smart contract
+    /// @param name Name of the token
+    /// @param symbol Symbol of the token
+    /// @param baseTokenURI Base URI of the token
+    constructor(
+        address admin,
+        string memory name,
+        string memory symbol,
+        string memory baseTokenURI,
+        address soulboundIdentity,
+        PaymentParams memory paymentParams
+    ) MasaSBTAuthority(admin, name, symbol, baseTokenURI, soulboundIdentity, paymentParams) {}
+
+    /// @notice Mints a new SBT
+    /// @dev The caller must have the MINTER role
+    /// @param to Address of the owner of the new identity
+    /// @return The NFT ID of the newly minted SBT
+    function mint(address paymentMethod, address to) public returns (uint256) {
+        uint256 tokenId = _mintWithCounter(paymentMethod, to);
+
+        emit Minted(tokenId, to);
+
+        return tokenId;
+    }
+
+    event Minted(uint256 tokenId, address to);
+}
